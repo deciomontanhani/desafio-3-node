23cec8bf50f906697aa0fc028bbfd0b2e8b2b5df
decio.montanhani <decio.montanhani@iteris.com.br>
2020-06-08 01:22:06 -0300

    finalizando o desafio com os testes passando

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    category,
+    title,
+    type,
+    value,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError(
+        'cannot be able to create outcome transaction without a valid balance',
+      );
+    }
+
+    let categoryObj = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryObj) {
+      categoryObj = categoryRepository.create({ title: category });
+      await categoryRepository.save(categoryObj);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      category: categoryObj,
+      value,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('this transaction does not exists.');
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
